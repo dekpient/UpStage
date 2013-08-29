852be874654a389e8f74cb6aef636428c70932ea
Nitkalya Wiriyanuparb <dekpient@gmail.com>
2013-08-29 21:30:53 +1200

    Add making bin directory before compiling

--- compile-swf.sh
+++ compile-swf.sh
@@ -3,6 +3,8 @@
 DEBUG=upstage.client.App.debug
 #DEBUG=no
 
+mkdir client/bin
+
 echo 'Compiling...'
 mtasc -version 10 -trace $DEBUG -msvc -wimp -strict -frame 1 -swf client/bin/classes.swf -header 320:200:31 -cp client/src client/src/App.as client/src/upstage/Client.as
 
